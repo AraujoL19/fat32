30eba10c81debfce02b1ce6a2848ff6cb252048a
Lucas Araujo de Oliveira <lucasaraujo.oliveira@estudante.ufjf.br>
2025-08-05 11:55:48 -0300

    mudanças na main

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
-/* 
-    Shell interativo
-*/
+#include "fat_manager.hpp"
+#include "shell.hpp"
+#include <iostream>
+#include <filesystem>
 
 int main() {
-    while (true) {
-        //TODO: le comandos do usuário, interpreta e chama as funções
+    if (!std::filesystem::exists(FAT_FILENAME)) {
+        std::cout << "Inicializando novo sistema de arquivos...\n";
+        init_fs();
+    } else {
+        load_fat();
     }
+
+    shell_loop();
     return 0;
 }
