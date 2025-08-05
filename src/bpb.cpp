56e508f3606899134593d932d7e2004c29bcd456
Lucas Araujo de Oliveira <lucasaraujo.oliveira@estudante.ufjf.br>
2025-08-04 22:34:31 -0300

    correcting read and print

--- src/bpb.cpp
+++ src/bpb.cpp
@@ -6,26 +6,26 @@
 
 bool readBPB(std::ifstream &img, BPB &bpb)
 {
-    img.seekg(0); // Move o "ponteiro de leitura" para a posição 0 (início do arquivo).
-    img.read(reinterpret_cast<char*>(&bpb), sizeof(BPB)); //Lê sizeof(BPB) bytes do arquivo e armazena na variável bpb.
-    return img.good(); // Retorna true se a leitura foi bem-sucedida, caso contrário, retorna false.
+    img.seekg(0);
+    img.read(reinterpret_cast<char*>(&bpb), sizeof(BPB));
+    return img.gcount() == sizeof(BPB);
 }
 
 void printBPB(const BPB &bpb)
 {
-    std::cout << "Jump Instruction: " << std::hex << std::setw(2) << std::setfill('0') // muda a saída de números inteiros para base hexadecimal e preenche com zeros à esquerda
+    std::cout << "Jump Instruction: " << std::hex << std::setw(2) << std::setfill('0')
               << static_cast<int>(bpb.BS_jmpBoot[0]) << " "
               << static_cast<int>(bpb.BS_jmpBoot[1]) << " "
               << static_cast<int>(bpb.BS_jmpBoot[2]) << std::dec << std::endl;
-    std::cout << "OEM Name: " << std::string(bpb.BS_OEMName) << std::endl;
+
+    std::cout << "OEM Name: " << std::string(bpb.BS_OEMName, 8) << std::endl;
     std::cout << "Bytes Per Sector: " << bpb.BS_BytsPerSec << std::endl;
     std::cout << "Sectors Per Cluster: " << static_cast<int>(bpb.BS_SecPerClus) << std::endl;
     std::cout << "Reserved Sectors: " << bpb.BS_RsvdSecCnt << std::endl;
     std::cout << "Number of FATs: " << static_cast<int>(bpb.BS_NumFATs) << std::endl;
     std::cout << "Root Entries: " << bpb.BS_RootEntCnt << std::endl;
     std::cout << "Total Sectors (16-bit): " << bpb.BS_TotSec16 << std::endl;
-    std::cout << "Media Type: 0x" << std::hex
-              << static_cast<int>(bpb.BS_Media) << std::dec << std::endl;
+    std::cout << "Media Type: 0x" << std::hex << static_cast<int>(bpb.BS_Media) << std::dec << std::endl;
     std::cout << "FAT Size (16-bit): " << bpb.BS_FATSz16 << std::endl;
     std::cout << "Sectors Per Track: " << bpb.BS_SecPerTrk << std::endl;
     std::cout << "Number of Heads: " << bpb.BS_NumHeads << std::endl;
