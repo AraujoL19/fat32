2acebb6fb2327ea5408686288fd57f9d75ea3ed7
Lucas Araujo de Oliveira <lucasaraujo.oliveira@estudante.ufjf.br>
2025-08-04 22:20:07 -0300

    bpb correction

--- src/bpb.cpp
+++ src/bpb.cpp
@@ -2,6 +2,7 @@
 #include "bpb.hpp"
 #include <iostream>
 #include <iomanip>
+#include <string>
 
 bool readBPB(std::ifstream &img, BPB &bpb)
 {
@@ -13,23 +14,30 @@ bool readBPB(std::ifstream &img, BPB &bpb)
 void printBPB(const BPB &bpb)
 {
     std::cout << "Jump Instruction: " << std::hex << std::setw(2) << std::setfill('0') // muda a saída de números inteiros para base hexadecimal e preenche com zeros à esquerda
-              << static_cast<int>(bpb.jump[0]) << " "
-              << static_cast<int>(bpb.jump[1]) << " "
-              << static_cast<int>(bpb.jump[2]) << std::dec << std::endl;
-    std::cout << "OEM Name: " << bpb.oem_name << std::endl;
-    std::cout << "Bytes Per Sector: " << bpb.bytes_per_sector << std::endl;
-    std::cout << "Sectors Per Cluster: " << static_cast<int>(bpb.sectors_per_cluster) << std::endl;
-    std::cout << "Reserved Sectors: " << bpb.reserved_sectors << std::endl;
-    std::cout << "Number of FATs: " << static_cast<int>(bpb.num_fats) << std::endl;
-    std::cout << "Root Entries: " << bpb.root_entries << std::endl;
-    std::cout << "Total Sectors (16-bit): " << bpb.total_sectors_short << std::endl;
+              << static_cast<int>(bpb.BS_jmpBoot[0]) << " "
+              << static_cast<int>(bpb.BS_jmpBoot[1]) << " "
+              << static_cast<int>(bpb.BS_jmpBoot[2]) << std::dec << std::endl;
+    std::cout << "OEM Name: " << std::string(bpb.BS_OEMName) << std::endl;
+    std::cout << "Bytes Per Sector: " << bpb.BS_BytsPerSec << std::endl;
+    std::cout << "Sectors Per Cluster: " << static_cast<int>(bpb.BS_SecPerClus) << std::endl;
+    std::cout << "Reserved Sectors: " << bpb.BS_RsvdSecCnt << std::endl;
+    std::cout << "Number of FATs: " << static_cast<int>(bpb.BS_NumFATs) << std::endl;
+    std::cout << "Root Entries: " << bpb.BS_RootEntCnt << std::endl;
+    std::cout << "Total Sectors (16-bit): " << bpb.BS_TotSec16 << std::endl;
     std::cout << "Media Type: 0x" << std::hex
-              << static_cast<int>(bpb.media_type) << std::dec << std::endl;
-    std::cout << "FAT Size (16-bit): " << bpb.fat_size_short << std::endl;
-    std::cout << "Sectors Per Track: " << bpb.sectors_per_track << std::endl;
-    std::cout << "Number of Heads: " << bpb.heads << std::endl;
-    std::cout << "Hidden Sectors: " << bpb.hidden_sectors << std::endl;
-    std::cout << "Total Sectors (32-bit): " 
-              << (bpb.total_sectors_short == 0 ? bpb.total_sectors_long : bpb.total_sectors_short) 
+              << static_cast<int>(bpb.BS_Media) << std::dec << std::endl;
+    std::cout << "FAT Size (16-bit): " << bpb.BS_FATSz16 << std::endl;
+    std::cout << "Sectors Per Track: " << bpb.BS_SecPerTrk << std::endl;
+    std::cout << "Number of Heads: " << bpb.BS_NumHeads << std::endl;
+    std::cout << "Hidden Sectors: " << bpb.BS_HiddSec << std::endl;
+    std::cout << "Total Sectors (32-bit): "
+              << (bpb.BS_TotSec16 == 0 ? bpb.BS_TotSec32 : bpb.BS_TotSec16)
               << std::endl;
+    std::cout << "FAT Size (32-bit): " << bpb.BS_FATSz32 << std::endl;
+    std::cout << "Root Cluster: " << bpb.BS_RootClus << std::endl;
+    std::cout << "FS Info Sector: " << bpb.BS_FSInfo << std::endl;
+    std::cout << "Backup Boot Sector: " << bpb.BS_BkBootSec << std::endl;
+    std::cout << "Volume ID: " << bpb.BS_VolID << std::endl;
+    std::cout << "Volume Label: " << std::string(bpb.BS_VolLab, 11) << std::endl;
+    std::cout << "Filesystem Type: " << std::string(bpb.BS_FileSysType, 8) << std::endl;
 }
--- src/shell.cpp
+++ src/shell.cpp
@@ -1,37 +1,37 @@
 #include "../include/shell.hpp"
 
+//TODO: init - inicializar o sistema de arquivos com as estruturas de dados, semelhante a formatar o sistema de arquivos virtual
 void init() {
-    //TODO: init - inicializar o sistema de arquivos com as estruturas de dados, semelhante a formatar o sistema de arquivos virtual
 }
 
+//TODO: load - carregar o sistema de arquivos do disco
 void load() {
-    //TODO: load - carregar o sistema de arquivos do disco
 }
 
+//TODO:  ls [/caminho/diretorio] - listar diretório
 void ls() {
-    //TODO:  ls [/caminho/diretorio] - listar diretório
 }
 
+//TODO: mkdir [/caminho/diretorio] - criar diretório
 void mkdir() {
-    //TODO: mkdir [/caminho/diretorio] - criar diretório
 }
 
+//TODO:  create [/caminho/arquivo] - criar arquivo
 void create() {
-    //TODO:  create [/caminho/arquivo] - criar arquivo
 }
 
+//TODO: unlink  [/caminho/arquivo] - excluir arquivo ou diretório (o diretório precisa estar vazio)
 void unlink() {
-    //TODO: unlink  [/caminho/arquivo] - excluir arquivo ou diretório (o diretório precisa estar vazio)
 }
 
+//TODO:  write "string" [/caminho/arquivo] - escrever dados em um arquivo (sobrescrever dados)
 void write() {
-    //TODO:  write "string" [/caminho/arquivo] - escrever dados em um arquivo (sobrescrever dados)
 }
 
+//TODO: append "string" [/caminho/arquivo] - anexar dados em um arquivo
 void append() {
-    //TODO: append "string" [/caminho/arquivo] - anexar dados em um arquivo
 }
 
+//TODO: read [/caminho/arquivo] - ler o conteúdo de um arquivo
 void read() {
-    //TODO: read [/caminho/arquivo] - ler o conteúdo de um arquivo
 }
